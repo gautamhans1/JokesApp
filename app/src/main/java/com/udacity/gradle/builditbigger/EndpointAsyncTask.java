68231386e879557be91f3edaf88bc44ed4996fb5
Gautam <gautamhans1@gmail.com>
2017-08-18 17:01:58 +0530

    Deployed Endpoints on App Engine

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
@@ -23,20 +23,9 @@ public class EndpointAsyncTask extends AsyncTask<Context, Void, String> {
     @Override
     protected String doInBackground(Context... params) {
         if (myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new
-                    MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // ­ 10.0.2.2 is localhost's IP address in Android emulator
-                    // ­ turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?>
-                                                       abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new
+                    AndroidJsonFactory(), null)
+                    .setRootUrl("https://joketellingapp-177211.appspot.com/_ah/api/");
             // end options for devappserver
             myApiService = builder.build();
         }

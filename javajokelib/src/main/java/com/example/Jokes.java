cd81c04619d916d87761493c7ca5ef27bab86ed1
Gautam <gautamhans1@gmail.com>
2017-08-18 15:22:46 +0530

    Created a basic Java Library

---
+++ javajokelib/src/main/java/com/example/Jokes.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Jokes {
+
+    private String joke = "What is the difference between a snowman and a snowwoman?\n Snowballs.";
+
+    public String tellJoke(){
+        return joke;
+    }
+}
